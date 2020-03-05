ed9c93b30da11d7091c34aae08542eac90cbafab
MaxWS <maxdjob1@gmail.com>
2020-03-05 18:36:27 +1100

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import WriterList from "./components/WriterList/WriterList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  	<React.Fragment>
+  		<Navbar />
+  		<WriterList />
+    	<Footer />
+    </React.Fragment>
   );
 }
 
